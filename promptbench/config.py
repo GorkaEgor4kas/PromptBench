e52e856d5e5cee9569c5f86e2ad25b06790c3cb9
Egor <kostyrineg@mail.ru>
2026-06-20 18:40:00 +0300

    feat: added config.py
    - it loads secrets from .env and creates clients for each LLM in the .env

---
+++ promptbench/config.py
@@ -0,0 +1,40 @@
+import os 
+from pydantic import BaseModel
+from openai import AsyncOpenAI
+
+class LLMConfig(BaseModel):
+    name: str
+    key: str
+    base_url: str
+    model: str
+
+def load_llm_config() -> list[LLMConfig]:
+    """
+    Loads all LLM_* variables from .env and returns a list of configs
+    """
+
+    configs = []
+    i = 1 
+
+    while True:
+        name = os.getenv(f"LLM_{i}_NAME")
+        if not name:
+            break
+
+        configs.append(LLMConfig(
+            name = name,
+            key = os.getenv(f"LLM_{i}_KEY", ""),
+            base_url = os.getenv(f"LLM_{i}_BASE_URL", "https://api.openai.com/v1"),
+            model = os.getenv(f"LLM_{i}_KEY", "")
+        ))
+        i += 1
+
+    return configs
+
+def create_clients(configs: list[LLMConfig]) -> dict[str, AsyncOpenAI]:
+    """
+    Creates clients for each config 
+    """
+    return {
+        cfg.name: AsyncOpenAI(api_key=cfg.key, base_url=cfg.base_url) for cfg in configs
+    }
